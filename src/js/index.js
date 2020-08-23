286d2a7d34d116ed652f298649027bc81fe09bc0
Michal Samulak <sanchossjmistik@gmail.com>
2020-08-23 19:58:13 +0100

    draw canvas

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,27 @@ import '../scss/main.scss';
 
 /* place your code below */
 
+
+
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+
+const width = 800;
+const height = 600;
+
+const engine = Engine.create();
+const { world } = engine;
+const render = Render.create({
+    element: document.body,
+    engine,
+    options: {
+        wireframes: false,
+        width,
+        height
+    }
+});
+Render.run(render);
+Runner.run(Runner.create(), engine);
+
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}))
